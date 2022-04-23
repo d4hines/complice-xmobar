cadddcdfea5f15b6dc3ebe221fa6242efd575992
Daniel Hines <d4hines@gmail.com>
2022-04-23 17:58:52 -0400

    switch to dream2nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,12 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    npm-build-package.url = "github:serokell/nix-npm-buildpackage";
-  };
-  outputs = { self, nixpkgs, flake-utils, npm-build-package }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ npm-build-package.overlay ];
-      }; in
-      let nodeModules = pkgs.mkNodeModules {
-        src = ./.;
-        pname = "my-node-modules";
-        version = "0.0.0";
-        packageOverrides = { };
+  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  outputs = { self, dream2nix }@inputs:
+    let
+      dream2nix = inputs.dream2nix.lib.init {
+        # modify according to your supported systems
+        systems = [ "x86_64-linux" "aarch64-linux" ];
+        config.projectRoot = ./.;
       };
-      in
-      rec {
-        packages.complice-xmobar =
-          pkgs.stdenv.mkDerivation
-            {
-              name = "complice-xmobar";
-              src = ./.;
-              buildPhase = "cp ${./complice.js} complice.js";
-              installPhase = ''
-                mkdir -p $out/app
-                cp -r ${nodeModules}/node_modules $out/app/node_modules
-                cp complice.js $out/app
-              '';
-            };
-      });
+    in
+    dream2nix.makeFlakeOutputs { source = ./.; };
 }
